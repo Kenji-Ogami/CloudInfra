2c9db03a5d0d4f612b4098475dea8d6938bf4ff3
Kenji Ogami <a2109ko@aiit.ac.jp>
2021-11-10 23:56:09 +0900

    Add dispatch kvm image by SCP

--- app/models/instance.rb
+++ app/models/instance.rb
@@ -1,2 +1,15 @@
 class Instance < ApplicationRecord
+    def send_instance(name:, machine_id:, cpu:, memory:, disk_space:, key_id:)
+        machine = Machine.find(machine_id)
+        option = {key: '/home/kenji/.ssh/key_local',
+                  poar : 22}
+        ip_addr = machine[:ip_addr]
+        user = 'kenji'
+
+        Net::SSH.start(ip_addr, user, option) do |ssh|
+            src = '/var/kvm/disk/kvm_centos8_bkup/disk.qcow2'
+            dist = '/home/kenji/'
+            ssh.scp.download! src, dist
+        end
+    end
 end
